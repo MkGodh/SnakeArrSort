b6f222dc1532390164adc448b12cc60664cc452c
MkaGodh <fenryrvla@yandex.ru>
2024-06-23 22:19:20 +0300

    Добавьте файлы проекта.

---
+++ SnakeArrSort/SnakeArrSort.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int snake[5][5];
+
+    int number = 0;
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            snake[i][j] = number;
+            number++;
+
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << snake[i][(i % 2 == 0) ? j : 5 - j - 1] << " ";
+        }
+        cout << endl;
+    }
+
+}
